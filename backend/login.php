56e5bca8717e2a4fdebc9063db49d16310957b27
katlegoprince98 <CynicsTech1998@gmail.com>
2023-05-24 11:05:26 +0200

    login

--- backend/login.php
+++ backend/login.php
@@ -1,48 +1,5 @@
 <?php
-<<<<<<< HEAD
-   //get connection string
-   require_once("config.php");
 
-
-
-   //start session
-   session_start();
-  
-   //code the login button
-
-   if(isset($_POST["submit"])){
- 
-     //create a function lto filter values from the form
-
-     function input_filter($data){
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-     }
-
-    //get values from the form
-    $email = input_filter($_POST["email"]);
-    $password = input_filter($_POST["password"]);
-    
-    $stmt = $conn->prepare("SELECT * FROM employees WHERE email =?");
-    $stmt->execute([$email]);
-
-    if($stmt->rowCount()=== 1){
-        $user = $stmt->fetch();
-
-        $user_email = $user['empemail'];
-        $user_password = $user['emppassword'];
-        $user_role = $user['emprole'];
-
-        
-    }
-   
-    
-    
-   }
-
-=======
  require("config.php");
 
  session_start();
@@ -77,6 +34,5 @@
 
  
 
->>>>>>> f2e7df2973728f83b5533b95372518657152cb0e
 
 ?>
